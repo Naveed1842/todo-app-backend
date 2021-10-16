4932b8ec012d3696c7a862ec62bd1e05f280924f
Muhammad Naveed <naveed4034@gmail.com>
2021-10-16 23:49:23 +0500

    red test cases are added for todo service

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TodoRepository } from '../todo-list/todo.repository';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
-  let service: TodoService;
+  let todoService;
+  let todoRepository;
+  const mockTodoRepository = () => ({
+    createTodo: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: TodoRepository,
+          useFactory: mockTodoRepository,
+        },
+      ],
     }).compile();
-
-    service = module.get<TodoService>(TodoService);
+    todoService = await module.get<TodoService>(TodoService);
+    todoRepository = await module.get<TodoRepository>(TodoRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createTodo', () => {
+    it('should save a todo in the database', async () => {
+      todoRepository.createTodo.mockResolvedValue('someTodo');
+      expect(todoRepository.createTodo).not.toHaveBeenCalled();
+      const createTodoDto = {
+        name: 'first todo',
+        description: 'first todo description',
+        status: 'complete',
+      };
+      const result = await todoService.createTodo(createTodoDto);
+      expect(todoRepository.createTodo).toHaveBeenCalledWith(createTodoDto);
+      expect(result).toEqual('someTodo');
+    });
+  });
+
+  describe('getTodo', () => {
+    it('should get all todo', async () => {
+      todoRepository.find.mockResolvedValue('todo');
+      expect(todoRepository.find).not.toHaveBeenCalled();
+      const result = await todoService.getTodo();
+      expect(todoRepository.find).toHaveBeenCalled();
+      expect(result).toEqual('todo');
+    });
   });
 });
