92d5c49caa10aa36cec465c36bf2f52751ab798f
Muhammad Naveed <naveed4034@gmail.com>
2021-10-16 23:52:50 +0500

    green test cases are added for todo service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from '../todo-list/todo.entity';
+import { TodoRepository } from '../todo-list/todo.repository';
+import { CreateTodoDTO } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodoService {
-  
+  constructor(
+    @InjectRepository(TodoRepository)
+    private todoRepository: TodoRepository,
+  ) {}
 
+  public async createTodo(createProductDto: CreateTodoDTO): Promise<Todo> {
+    return await this.todoRepository.createTodo(createProductDto);
+  }
+
+  public async getTodo(): Promise<Todo[]> {
+    return await this.todoRepository.find();
+  }
 }
